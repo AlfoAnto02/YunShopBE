b55ac67df23cadab26c8c1cfd1a61eae259f4f98
SenseiBonsai2K <cristian.marinozzi1@gmail.com>
2025-02-14 18:27:09 +0100

    Implemented and organised base components, services and guard

--- YunShopFE/src/app/app.routes.ts
+++ YunShopFE/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'SignUp', component: SignUpComponent },
+    { path: 'SignIn', component: SignInComponent },
+    { path: 'Profile', component: ProfileComponent, canActivate: [authGuard] }
+];
